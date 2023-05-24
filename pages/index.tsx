dd4aa43c797f2a3e8ed324d54b944dd8ce5ca0a9
AmitShilo434 <132204571+AmitShilo434@users.noreply.github.com>
2023-05-24 14:57:53 +0300

    pagination

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,15 @@ import type { GetServerSideProps } from "next";
 import Layout from "../components/Layout";
 import Post, { PostProps } from "../components/Post";
 import prisma from '../lib/prisma'
+import Link from 'next/link';
 
-export const getServerSideProps: GetServerSideProps = async () => {
+const POST_PER_PAGE = 10
+
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  const page = parseInt(query.page as string) || 1;  // Default to page 1 if no page parameter is provided
+  const perPage = POST_PER_PAGE;
+  const skip = (page - 1) * perPage;
+
   const feed = await prisma.post.findMany({
     where: {
       published: true,
@@ -16,28 +23,47 @@ export const getServerSideProps: GetServerSideProps = async () => {
         },
       },
     },
+    skip,
+    take: perPage,
   });
+
   return {
-    props: { feed },
+    props: { feed, page },
   };
 };
 
+
 type Props = {
   feed: PostProps[];
+  page: number;
 };
 
 const Blog: React.FC<Props> = (props) => {
+  const { feed, page } = props;
+
   return (
     <Layout>
       <div className="page">
         <h1>Public Feed</h1>
         <main>
-          {props.feed.map((post) => (
+          {feed.map((post) => (
             <div key={post.id} className="post">
               <Post post={post} />
             </div>
           ))}
         </main>
+        <div className="pagination">
+          {page > 1 && (
+            <Link href={`/?page=${page - 1}`}>
+              &larr; Previous Page
+            </Link>
+          )}  
+          {feed.length === POST_PER_PAGE && (
+            <Link href={`/?page=${page + 1}`}>
+              Next Page &rarr;
+            </Link>
+          )}
+        </div>
       </div>
       <style jsx>{`
         .post {
@@ -52,9 +78,16 @@ const Blog: React.FC<Props> = (props) => {
         .post + .post {
           margin-top: 2rem;
         }
+
+        .pagination {
+          margin-top: 2rem;
+          display: flex;
+          justify-content: space-between;
+        }
       `}</style>
     </Layout>
   );
 };
+
 
 export default Blog;
