2b6757f454c697713366cb9cc53602861f36b869
AmitShilo434 <132204571+AmitShilo434@users.noreply.github.com>
2023-06-01 03:12:05 +0300

    mongoose done

--- pages/api/post/index.ts
+++ pages/api/post/index.ts
@@ -3,6 +3,15 @@ import prisma from '../../../lib/prisma'
 import formidable from 'formidable';
 import PersistentFile from 'formidable/PersistentFile';
 
+// Disables automatic body parsing
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+
+// Configuration to cloudinary
 const cloudinary = require('cloudinary').v2;
 cloudinary.config({
   cloud_name: "dq0mokt4c",
@@ -10,16 +19,21 @@ cloudinary.config({
   api_secret: "wvfaZlTCnIab2Jx2XkRp-f-SxIQ"
 });
 
-
-export const config = {
-  api: {
-    bodyParser: false, // Disables automatic body parsing
-  },
-};
+// Configuration and connect to mongoose
+const mongoose = require('mongoose')
+const url =
+  `mongodb+srv://admin:vH0ceyRtSuZVaNDy@videos.m4cdbi1.mongodb.net/?retryWrites=true&w=majority`
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
+const videoSchema = new mongoose.Schema({
+  postId: String,
+  url: String,
+  user: String,
+  date: Date,
+})
+const VideoMD = mongoose.model('Video', videoSchema)
 
 // POST /api/post
-// Required fields in body: title
-// Optional fields in body: content
 export default async function handle(req: NextApiRequest, res: NextApiResponse) {
   console.log("in handle")
   const form = new formidable.IncomingForm();
@@ -34,20 +48,79 @@ export default async function handle(req: NextApiRequest, res: NextApiResponse) 
         return;
       }
 
-      // Handle the uploaded file here
-      const video: PersistentFile | any = files.video;
-      console.log("trying to uplosad: ", video.filepath)
-      const result = cloudinary.uploader.upload(video.filepath, {
-        resource_type: 'video',
-        folder: 'videos', // Specify the folder where you want to save the video file in Cloudinary
-      });
+      const title: any = fields.title;
+      const content: any = fields.content;
+      const email: any = fields.email;
 
-      result.then((data:  any) => {
-        // console.log(data);
-        console.log(data.secure_url);
-      }).catch((err: any) => {
-        console.log(err);
-      });
+      const postResult: any = prisma.post.create({
+      data: {
+        title: title,
+        content: content,
+        author: { connect: { email: email } },
+        },
+      }).then((postResult) => {
+        const postId = postResult.id;
+        console.log(postId); // Assuming postId exists in the result
+
+        // Handle the uploaded file here
+        if(files.video) {
+          const video: PersistentFile | any = files.video;
+          console.log("trying to upload: ", video.filepath)
+          const result = cloudinary.uploader.upload(video.filepath, {
+            resource_type: 'video',
+            folder: 'videos', // Specify the folder where you want to save the video file in Cloudinary
+          });
+
+          result.then((data:  any) => {
+            // console.log(data);
+            console.log(data.secure_url);
+
+            
+            const video = new VideoMD({
+              postId: postResult.id,
+              url: data.secure_url,
+              user: postResult.authorId,
+              date: new Date(),
+            });
+            video.save().then((res: any) => {
+              console.log('video saved!', res.id)
+              mongoose.connection.close()
+            })
+
+            // const video = new Video({
+            //   postId: postId,
+            //   url: data.secure_url,
+            //   user: "sa",
+            // });
+            // video.save().then(result => {
+            //   console.log('video saved!')
+            //   mongoose.connection.close()
+            // })
+
+
+          }).catch((err: any) => {
+            console.log(err);
+          });
+        }
+
+      })
+        // res.json(result);
+
+
+      // Handle the uploaded file here
+      // const video: PersistentFile | any = files.video;
+      // console.log("trying to uplosad: ", video.filepath)
+      // const result = cloudinary.uploader.upload(video.filepath, {
+      //   resource_type: 'video',
+      //   folder: 'videos', // Specify the folder where you want to save the video file in Cloudinary
+      // });
+
+      // result.then((data:  any) => {
+      //   // console.log(data);
+      //   console.log(data.secure_url);
+      // }).catch((err: any) => {
+      //   console.log(err);
+      // });
     } else {
     res.status(401).send({ message: 'Unauthorized' })
   }
